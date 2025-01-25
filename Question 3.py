c8e222fedfbd33b646013c7f264670f72b8bf077
Coder Shota Kun <kywzinth3nt@gmail.com>
2025-01-26 04:58:32 +0630

    Update Question 3.py

--- Question 3.py
+++ Question 3.py
@@ -10,7 +10,7 @@ def main(params):
     port = params.port
     db = params.db
     table_name = params.table_name
-    csv_name = 'yellow_tripdata_2021-01.csv'  # Make sure to provide the correct path
+    csv_name = 'green_tripdata_2019-10.csv'  # Correct file name for the green taxi data
 
     # Establish the connection to PostgreSQL
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
@@ -20,8 +20,8 @@ def main(params):
 
     # Process the first chunk
     df = next(df_iter)
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
     # Create table if not exists and load the first chunk
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
@@ -31,15 +31,15 @@ def main(params):
     while True:
         t_start = time()
         df = next(df_iter)
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
         df.to_sql(name=table_name, con=engine, if_exists='append')
 
         t_end = time()
         print(f'Inserted another chunk... took {t_end - t_start:.3f} seconds')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV into Postgres')
+    parser = argparse.ArgumentParser(description='Ingest Green Taxi Data into Postgres')
 
     parser.add_argument('--user', help='User name for Postgres')
     parser.add_argument('--password', help='Password for Postgres')
